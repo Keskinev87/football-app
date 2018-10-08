d830dc968d687386db2f7021803825f53e8b18a4
Yavor Keskinev <yavor_keskinev@abv.bg>
2018-10-09 00:33:11 +0300

    Small Updates to the Game Model

--- src/app/components/models/game.model.ts
+++ src/app/components/models/game.model.ts
@@ -3,6 +3,7 @@ export class Game {
     constructor(
     public id: string,
     public name: string,
+    public type: string,
     public shareableUrl: string,
     public secretCode: number,
     public leagues: Array<object>,
