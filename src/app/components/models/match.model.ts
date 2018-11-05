a6f204bd5b1eeb010654017dbfc85f2670ff7340
Yavor Keskinev <yavor_keskinev@abv.bg>
2018-11-05 23:23:49 +0200

    Match model fix

--- src/app/components/models/match.model.ts
+++ src/app/components/models/match.model.ts
@@ -1,46 +1,15 @@
 export class Match {
 	constructor(public id: number,public competition: {id: number;name: string;},
-		public season: {
-			id: number;
-			startDate: Date;
-			endDate: Date;
-			currentMatchday: number;
-			availableStages?: Array<string>;
-		},
+		public season: Object,
 		public utcDate: Date,
 		public status: string,
 		public matchday: number,
 		public stage: string,
 		public group: string,
 		public lastUpdated: Date,
-		public homeTeam: {
-			id: number;
-			name: string;
-		},
-		public awayTeam: {
-			id: number;
-			name: string;
-		},
-		public score: {
-			winner: string,
-			duration: string,
-			fullTime: {
-				homeTeam: number;
-				awayTeam: number;
-			},
-			halfTime: {
-				homeTeam: number;
-				awayTeam: number;
-			},
-			extraTime: {
-				homeTeam: number;
-				awayTeam: number;
-			},
-			penalties: {
-				homeTeam: number;
-				awayTeam: number;
-			}
-		},
+		public homeTeam: Object,
+		public awayTeam: Object,
+		public score: Object,
 		public referees?: Array<object>,
 		public goals?: Array<object>,
 		public bookings?: Array<object>,
