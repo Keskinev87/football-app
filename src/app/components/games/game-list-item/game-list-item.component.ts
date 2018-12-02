fa4f8520793c81113b3adb0daeb4efeb356a91a1
Yavor Keskinev <yavor_keskinev@abv.bg>
2018-12-02 16:43:05 +0200

    The administrative buttons for games will only show if the user is admin now

--- src/app/components/games/game-list-item/game-list-item.component.ts
+++ src/app/components/games/game-list-item/game-list-item.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import * as fromApp from '../../../app.reducers'
 import * as GameActions from '../games-store/games.actions'
 import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-game-list-item',
@@ -12,11 +13,21 @@ import { Router } from '@angular/router';
 })
 export class GameListItemComponent implements OnInit {
   @Input() game: Game
+  @Input() user: User
+  isAdmin: boolean
 
   constructor(private store: Store<fromApp.AppState>, private router: Router ) { }
 
 
   ngOnInit() {
+    console.log(this.game.admin)
+    console.log(typeof(this.game.admin))
+    console.log(this.user._id)
+    console.log(typeof(this.user._id))
+    if (this.game.admin == this.user._id)
+      this.isAdmin = true
+    else
+      this.isAdmin = false
   }
 
   onChooseCompetitions() {
