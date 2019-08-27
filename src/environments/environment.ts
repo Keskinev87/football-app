65e27a7f6ba4d61092f697117cfaa8893ce465bc
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-08-27 14:12:37 +0300

    fix prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: "http://localhost:3000"
+  production: true,
+  apiUrl: 'https://damp-caverns-37412.herokuapp.com'
 };
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
